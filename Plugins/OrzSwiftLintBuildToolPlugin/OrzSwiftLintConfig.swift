d0f38ce63822f38ef2bb3426e1fd768bfb75e899
wangzhizhou <wangzhizhou@bytedance.com>
2023-05-25 18:58:18 +0800

    fix error when used in xcode target and setup rule config

--- Plugins/OrzSwiftLintBuildToolPlugin/OrzSwiftLintConfig.swift
+++ Plugins/OrzSwiftLintBuildToolPlugin/OrzSwiftLintConfig.swift
@@ -5,7 +5,9 @@
 //  Created by joker on 2023/5/25.
 //
 //  - [SwiftLint in Depth](https://www.kodeco.com/38422105-swiftlint-in-depth)
-//  - [SwiftLint](https://swiftpackageindex.com/realm/SwiftLint)
+//  - [SwiftLint](https://github.com/realm/SwiftLint)
+//  - [SwiftLintDoc](https://realm.github.io/SwiftLint/)
+//  - [raywenderlich rules](https://github.com/kodecocodes/swift-style-guide/blob/main/com.raywenderlich.swiftlint.yml)
 
 import Foundation
 import PackagePlugin
@@ -27,29 +29,45 @@ struct OrzSwiftLintConfig {
     
     private let configName = "OrzSwiftLintConfig.yml"
 
-    /// Reference
-    /// [raywenderlich rules](https://github.com/kodecocodes/swift-style-guide/blob/main/com.raywenderlich.swiftlint.yml)
+    /// [SwiftLintRuleDirectory](https://realm.github.io/SwiftLint/rule-directory.html)
     private let configYamlFormatContent = """
 
+disabled_rules:
+  - identifier_name
+
 opt_in_rules:
-    - indentation_width
+  - indentation_width
 
 trailing_whitespace:
-    ignores_empty_lines: true
+  ignores_empty_lines: true
 
 vertical_whitespace:
-    max_empty_lines: 2
+  max_empty_lines: 2
 
 indentation_width:
-    indentation_width: 4
+  indentation_width: 4
+  include_compiler_directives: false
 
 line_length:
+  warning: 120
+  error: 300
   ignores_urls: true
   ignores_function_declarations: true
   ignores_comments: true
 
 analyzer_rules:
   - unused_import
+
+identifier_name:
+  excluded:
+    - i
+    - id
+    - x
+    - y
+    - z
+
+type_name:
+    validates_start_with_lowercase: warning
 
 """
 }
